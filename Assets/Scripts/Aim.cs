e23b29cf39891cdeb6bb13cc4c6ccf6748dd24d2
SUSSEXDOWNS\476594 <476594@chrome.sussexdowns.ac.uk>
2018-12-04 12:20:18 +0000

    2 save

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
-
+    Rigidbody rigidB;
+    [SerializeField]
+    Camera cam;
 	// Use this for initialization
 	void Start () {
-		
+        rigidB = GetComponent<Rigidbody>();
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    void AimLogic()
+    {
+        float _leftRightValue = Input.GetAxisRaw("Mouse X");
+        float _upDownValue = Input.GetAxisRaw("Mouse Y");
+        Vector3 _rotationX = new Vector3(_upDownValue, 0, 0);
+        Vector3 _rotationY = new Vector3(0, _leftRightValue, 0);
+        rigidB.MoveRotation(rigidB.rotation * Quaternion.Euler(_rotationY));
+        cam.transfer.
+
+    }
 }
